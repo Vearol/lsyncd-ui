7c3f910e8de7b7c282d0c73a44354248d74edbcd
Taras Kushnir <kushnirTV@gmail.com>
2016-07-12 21:11:01 +0300

    Added missing files

---
+++ lsyncd-ui-tests/filesystemtreetest.cpp
@@ -0,0 +1,82 @@
+#include "filesystemtreetest.h"
+#include "../lsyncd_ui/filesystemtree.h"
+
+void FileSystemTreeTest::simpleAddPathTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    QVERIFY(tree.isFullBackup("/a/b/c/"));
+    QVERIFY(!tree.isPartialBackup("/a/b/c"));
+}
+
+void FileSystemTreeTest::simplePartialBackupTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    QVERIFY(!tree.isFullBackup("/a"));
+    QVERIFY(!tree.isFullBackup("/a/b/"));
+    QVERIFY(tree.isPartialBackup("/a/b"));
+}
+
+void FileSystemTreeTest::addPathsTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    tree.addBackupPath("/a/b/d");
+
+    QVERIFY(tree.isFullBackup("/a/b/c/"));
+    QVERIFY(tree.isFullBackup("/a/b/d/"));
+}
+
+void FileSystemTreeTest::addSiblingsPathsTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/c");
+    tree.addBackupPath("/a/d");
+    tree.addBackupPath("/a/b/c");
+    tree.addBackupPath("/a/b/d");
+
+    QVERIFY(!tree.isFullBackup("/a"));
+    QVERIFY(tree.isPartialBackup("/a"));
+    QVERIFY(tree.isFullBackup("/a/c"));
+    QVERIFY(tree.isFullBackup("/a/d"));
+    QVERIFY(tree.isFullBackup("/a/b/c/"));
+    QVERIFY(tree.isFullBackup("/a/b/d/"));
+}
+
+void FileSystemTreeTest::isBackedUpWithParentTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b");
+
+    QVERIFY(tree.isFullBackup("/a/b/c/"));
+}
+
+void FileSystemTreeTest::addMoreCommonItemTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    tree.addBackupPath("/a/b/");
+
+    QVERIFY(tree.isFullBackup("/a/b/"));
+}
+
+void FileSystemTreeTest::removePathsTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    tree.removeBackupPath("/a/b/c");
+
+    QVERIFY(!tree.isFullBackup("/a/b/c"));
+    QVERIFY(!tree.isPartialBackup("/a/b/c"));
+
+    QVERIFY(!tree.isFullBackup("/a"));
+    QVERIFY(!tree.isPartialBackup("/a"));
+}
+
+void FileSystemTreeTest::removeCleanupTest() {
+    BackupTree tree;
+    tree.addBackupPath("/a/b/c");
+    tree.addBackupPath("/a/k/r");
+
+    tree.removeBackupPath("/a/b/c");
+
+    QVERIFY(!tree.isFullBackup("/a/b/"));
+    QVERIFY(!tree.isPartialBackup("/a/b/"));
+
+    QVERIFY(tree.isFullBackup("/a/k/r/"));
+    QVERIFY(tree.isPartialBackup("/a/k"));
+}
---
+++ lsyncd_ui/filesystemtree.h
@@ -0,0 +1,43 @@
+#ifndef FILESYSTEMTREE_H
+#define FILESYSTEMTREE_H
+
+#include <QString>
+#include <vector>
+
+class BackupTreeNode {
+public:
+    BackupTreeNode(const QString &value):
+        m_Value(value),
+        m_Child(nullptr),
+        m_Sibling(nullptr)
+    { }
+
+public:
+    QString m_Value;
+    BackupTreeNode *m_Child;
+    BackupTreeNode *m_Sibling;
+};
+
+class BackupTree
+{
+public:
+    BackupTree();
+    ~BackupTree();
+
+public:
+    void addBackupPath(const QString &path);
+    void removeBackupPath(const QString &path);
+    bool isFullBackup(const QString &path) const;
+    bool isPartialBackup(const QString &path) const;
+
+private:
+    BackupTreeNode *findNode(const QString &path, bool &processedAll) const;
+    void doDeleteNode(BackupTreeNode *nodeToLookIn, BackupTreeNode *parent,
+                                 const QStringList &path, int currIndex);
+    void freeNode(BackupTreeNode *nodeToDelete, BackupTreeNode *prevSibling, BackupTreeNode *parent);
+
+private:
+    BackupTreeNode *m_Root;
+};
+
+#endif // FILESYSTEMTREE_H
